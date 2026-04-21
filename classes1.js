4a2c037595d22d4e1b0c54c7800d06e4c148248b
Vishwas <122880819+Vishwas-Dev@users.noreply.github.com>
2026-04-21 20:32:07 +0530

    Update Product class with private price and validation
    Refactor Product class to use private field for price and add validation in setter.

--- classes1.js
+++ classes1.js
@@ -14,33 +14,45 @@ const phone = new Product( "vishal", 999, "this is New");
 
 console.log(phone);
 
-// setter and getter
+// ---------------------------------------------------------
 
+// setter and getter
 
 class Product {
   name;
-  price;
+  #price;
   description;
 
   constructor(name, price, description) {
     this.name = name;
-    this.price = price;
+    this.#price = price;
     this.description = description;
   }
  get price(){
-    return this.price;
+    return this.#price;
  }
      
- set price(p) {
-   this.price=p;
+//  set price(p) {
+//    this.#price=p;
+//   }
+
+//  in getter and setter setter → setter → setter → setter → ...
+// 👉 Infinite loop → crashes / doesn’t work so use -> this.#price = p;
+
+set price(p) {
+    if (p > 0) {
+        this.#price = p;
+    } else {
+        console.log("invalid value");
+    }
   }
 
-
 }
-const phone = new Product( "vishal", 999, "this is New");
+const phone = new Product( "vishal", 0, "this is New");
 
 console.log(phone);
-phone.price = 700;
+phone.price = -200; // condition fails p>0
 console.log("this is Price : ",phone.price);
 console.log(phone);
 
+// ---------------------------------------------------------
