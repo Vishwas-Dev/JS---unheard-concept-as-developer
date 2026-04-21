942e8639fb51db3015ab275b6c3850db15b237a0
Vishwas <122880819+Vishwas-Dev@users.noreply.github.com>
2026-04-21 20:17:19 +0530

    Implement getter and setter for Product price
    Added getter and setter for the price property in the Product class.

--- classes1.js
+++ classes1.js
@@ -13,3 +13,34 @@ class Product {
 const phone = new Product( "vishal", 999, "this is New");
 
 console.log(phone);
+
+// setter and getter
+
+
+class Product {
+  name;
+  price;
+  description;
+
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+ get price(){
+    return this.price;
+ }
+     
+ set price(p) {
+   this.price=p;
+  }
+
+
+}
+const phone = new Product( "vishal", 999, "this is New");
+
+console.log(phone);
+phone.price = 700;
+console.log("this is Price : ",phone.price);
+console.log(phone);
+
