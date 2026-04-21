78b8e85d0f7facbdff05afd6ede9eb747589a8dd
Vishwas <122880819+Vishwas-Dev@users.noreply.github.com>
2026-04-21 19:43:17 +0530

    Refactor Product class with constructor and properties
    Refactored Product class to use public properties and added constructor.

--- classes1.js
+++ classes1.js
@@ -1,8 +1,15 @@
-class Product{
-    #name;
-    #price;
-    #description;
-constructor(){
+class Product {
+  name;
+  price;
+  description;
+
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
 
 }
-}
+const phone = new Product( "vishal", 999, "this is New");
+
+console.log(phone);
