68fc5ef9ca27df9794c434bee382699ef0624a70
Vishwas <122880819+Vishwas-Dev@users.noreply.github.com>
2026-04-24 21:24:41 +0530

    Refactor  Product class with private price and validation
    Refactored Product class to use private price property with getter and setter. Added validation in setter to prevent invalid price assignment.

--- classes1.js
+++ classes1.js
@@ -57,4 +57,16 @@ console.log(phone);
 
 // ---------------------------------------------------------
 
+class Product {
+  
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+
+}
+const phone = new Product( "vishal", 999, "this is New");
+
+console.log(phone);
 
