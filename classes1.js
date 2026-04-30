3243d2c8e418aee97850dece917a6cdb5b212ef4
Vishwas <122880819+Vishwas-Dev@users.noreply.github.com>
2026-04-30 21:19:44 +0530

    added

--- classes1.js
+++ classes1.js
@@ -1,1 +1,87 @@
 
+class Product {
+  name;
+  price;
+  description;
+
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+
+}
+const phone = new Product( "vishal", 999, "this is New");
+
+console.log(phone);
+
+// ---------------------------------------------------------
+
+// setter and getter
+
+class Product {
+  name;
+  #price;
+  description;
+
+  constructor(name, price, description) {
+    this.name = name;
+    this.#price = price;
+    this.description = description;
+  }
+ get price(){
+    return this.#price;
+ }
+     
+//  set price(p) {
+//    this.#price=p;
+//   }
+
+//  in getter and setter setter → setter → setter → setter → ...
+// 👉 Infinite loop → crashes / doesn’t work so use -> this.#price = p;
+
+set price(p) {
+    if (p > 0) {
+        this.#price = p;
+    } else {
+        console.log("invalid value");
+    }
+  }
+
+}
+const phone = new Product( "vishal", 0, "this is New");
+
+console.log(phone);
+phone.price = -200; // condition fails p>0
+console.log("this is Price : ",phone.price);
+console.log(phone);
+
+
+// ---------------------------------------------------------
+
+class Product {
+  
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+
+}
+const phone = new Product( "vishal", 999, "this is New");
+
+// ---------------------------------------------------------
+
+class Product {
+  
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+
+}
+const phone = new Product( "vishal", 999, "this is New");
+
+console.log(phone);
+console.log(phone);
